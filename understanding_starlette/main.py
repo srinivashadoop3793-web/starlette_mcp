a8d9566c7033f2af5aa87130cd8d46a3c4dd1b0f
Srinivas M <srinivashadoop3793@gmail.com>
2025-12-23 13:47:33 +0530

    adding project

---
+++ understanding_starlette/main.py
@@ -0,0 +1,18 @@
+from ast import main
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+
+async def homepage(request):
+    return JSONResponse({"Hello": "World"})
+
+# async def otherpage(request):
+#     return JSONResponse({"other": "page"})
+
+app = Starlette(debug=True, routes=[
+    Route("/", homepage)])
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
